9525332ab544a9c230f7d3ad215e976fd11ca1c5
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-04-27 00:16:03 +0200

    Made the code more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::{self, Rng};
+use rand::Rng;
 use std::io;
 
 #[derive(PartialEq)]
@@ -7,47 +7,41 @@ enum Move {
     Down,
     Left,
     Right,
-    Invalid,
-}
-// Have to use &mut String, because it's for user input, which is modifiable by definition
-fn match_move(user_input: &mut String) -> Move {
-    // trim() returns &str, so it matches the type
-    match user_input.trim() {
-        "w" => Move::Up,
-        "s" => Move::Down,
-        "a" => Move::Left,
-        "d" => Move::Right,
-        _ => Move::Invalid,
-    }
 }
 
-fn make_move(mov: Move, board: &mut Board) {
-    match mov {
-        Move::Up => {
-            if board.player_position.row > 1 {
+impl Move {
+    // Have to use &mut String, because it's for user input, which is modifiable by definition
+    fn match_move(user_input: String) -> Option<Move> {
+        // trim() returns &str, so it matches the type
+        match user_input.trim() {
+            "w" => Some(Move::Up),
+            "s" => Some(Move::Down),
+            "a" => Some(Move::Left),
+            "d" => Some(Move::Right),
+            _ => None,
+        }
+    }
+
+    fn make_move(mov: Move, board: &mut Board) {
+        match mov {
+            Move::Up if board.player_position.row > 1 => {
                 board.player_position.row -= 1;
                 println!("You moved up!");
             }
-        }
-        Move::Down => {
-            if board.player_position.row < 6 {
+            Move::Down if board.player_position.row < 6 => {
                 board.player_position.row += 1;
                 println!("You moved down!");
             }
-        }
-        Move::Left => {
-            if board.player_position.col > 1 {
+            Move::Left if board.player_position.col > 1 => {
                 board.player_position.col -= 1;
                 println!("You moved left!");
             }
-        }
-        Move::Right => {
-            if board.player_position.col < 6 {
+            Move::Right if board.player_position.col < 6 => {
                 board.player_position.col += 1;
                 println!("You moved right!");
             }
+            _ => {}
         }
-        Move::Invalid => println!("Invalid move!"),
     }
 }
 
@@ -84,99 +78,98 @@ struct Board {
     player_position: Loc,
 }
 
-fn initialize_board() -> Board {
-    let board = Board {
-        field: vec![vec![Field::Empty; 8]; 8],
-        apples_positions: vec![],
-        player_position: Loc { row: 0, col: 0 },
-    };
-    board
-}
-
-fn generate_apples(board: &mut Board, rng: &mut rand::rngs::ThreadRng) {
-    while board.apples_positions.len() < 7 {
-        let row: usize = rng.gen_range(1..6);
-        let col: usize = rng.gen_range(1..6);
-        let current_apple_position = Loc { row, col };
-        if !board.apples_positions.contains(&current_apple_position) {
-            board.apples_positions.push(current_apple_position);
-        }
+impl Board {
+    fn initialize_board() -> Board {
+        let board = Board {
+            field: vec![vec![Field::Empty; 8]; 8],
+            apples_positions: vec![],
+            player_position: Loc { row: 0, col: 0 },
+        };
+        board
     }
-}
 
-fn generate_player(board: &mut Board, rng: &mut rand::rngs::ThreadRng) {
-    loop {
-        let row: usize = rng.gen_range(1..6);
-        let col: usize = rng.gen_range(1..6);
-
-        if !board.apples_positions.contains(&Loc { row, col }) {
-            board.player_position = Loc { row, col };
-            break;
-        }
-    }
-}
-
-fn update_board(board: &mut Board) {
-    for row in 0..8 {
-        for col in 0..8 {
-            let current_loc = Loc { row, col };
-            let should_be_wall = row == 0 || row == 7 || col == 0 || col == 7;
-
-            if current_loc == board.player_position {
-                board.field[row][col] = Field::Player;
-            } else if board.apples_positions.contains(&current_loc) {
-                board.field[row][col] = Field::Apple;
-            } else if should_be_wall {
-                board.field[row][col] = Field::Wall;
-            } else {
-                board.field[row][col] = Field::Empty;
+    fn generate_apples(&mut self, rng: &mut rand::rngs::ThreadRng) {
+        while self.apples_positions.len() < 7 {
+            let row: usize = rng.gen_range(1..6);
+            let col: usize = rng.gen_range(1..6);
+            let current_apple_position = Loc { row, col };
+            if !self.apples_positions.contains(&current_apple_position) {
+                self.apples_positions.push(current_apple_position);
             }
         }
     }
-}
 
-fn print_board(board: &Board) {
-    for row in &board.field {
-        for cell in row {
-            print!("{} ", cell.match_field());
+    fn generate_player(&mut self, rng: &mut rand::rngs::ThreadRng) {
+        loop {
+            let row: usize = rng.gen_range(1..6);
+            let col: usize = rng.gen_range(1..6);
+
+            if !self.apples_positions.contains(&Loc { row, col }) {
+                self.player_position = Loc { row, col };
+                break;
+            }
         }
-        println!();
     }
-}
 
-fn update_player_position(board: &mut Board) {
-    loop {
+    fn update_board(&mut self) {
+        for row in 0..8 {
+            for col in 0..8 {
+                let current_loc = Loc { row, col };
+                let should_be_wall = row == 0 || row == 7 || col == 0 || col == 7;
+
+                let matched_field = if current_loc == self.player_position {
+                    Field::Player
+                } else if self.apples_positions.contains(&current_loc) {
+                    Field::Apple
+                } else if should_be_wall {
+                    Field::Wall
+                } else {
+                    Field::Empty
+                };
+
+                self.field[row][col] = matched_field;
+            }
+        }
+    }
+
+    fn print_board(&self) {
+        for row in &self.field {
+            for cell in row {
+                print!("{} ", cell.match_field());
+            }
+            println!();
+        }
+    }
+
+    fn update_player_position(&mut self) {
         let mut user_input = String::new();
         io::stdin()
             .read_line(&mut user_input)
             .expect("Failed to read line");
-        let matched_move = match_move(&mut user_input);
-        if matched_move != Move::Invalid {
-            make_move(matched_move, board);
-            break;
+        if let Some(matched_move) = Move::match_move(user_input) {
+            Move::make_move(matched_move, self);
         }
     }
-}
-
-fn remove_apples(board: &mut Board) {
-    if board.apples_positions.contains(&board.player_position) {
-        // Idea taken from https://stackoverflow.com/questions/26243025/how-to-remove-an-element-from-a-vector-given-the-element
-        // Basically the same as chatGPT said:
-        // 1. Create an iterator of apples positions
-        // 2. Get the index of the apple the player is currently standing on
-        // 3. Remove the value from the index
-        let apple_to_be_eaten_index = board
+    // Idea taken from https://stackoverflow.com/questions/26243025/how-to-remove-an-element-from-a-vector-given-the-element
+    // 1. Create an iterator of apples positions
+    // 2. Get the index of the apple the player is currently standing on
+    // 3. Remove the value from the index
+    // if let works like this:
+    // if let Some(variable) = function_returning_something_or_nothing {code block that executes if the function returned something}
+    fn remove_apples(&mut self) {
+        if let Some(apple_to_be_eaten_index) = self
             .apples_positions
             .iter()
-            .position(|x| *x == board.player_position)
-            .unwrap();
-        board.apples_positions.remove(apple_to_be_eaten_index);
-        println!("Apple eaten!");
+            .position(|x| *x == self.player_position)
+        {
+            self.apples_positions.remove(apple_to_be_eaten_index);
+            println!("Apple eaten!");
+        }
     }
-}
 
-fn no_apples_left(board: &Board) -> bool {
-    board.apples_positions.len() == 0
+    fn no_apples_left(&self) -> bool {
+        self.apples_positions.is_empty()
+    }
 }
 
 fn print_congrats_and_restart() {
@@ -185,18 +178,18 @@ fn print_congrats_and_restart() {
 }
 
 fn main() {
-    let mut board = initialize_board();
+    let mut board = Board::initialize_board();
     let mut rng = rand::thread_rng();
 
-    generate_apples(&mut board, &mut rng);
-    generate_player(&mut board, &mut rng);
+    Board::generate_apples(&mut board, &mut rng);
+    Board::generate_player(&mut board, &mut rng);
 
     loop {
-        update_board(&mut board);
-        print_board(&board);
-        update_player_position(&mut board);
-        remove_apples(&mut board);
-        if no_apples_left(&board) {
+        Board::update_board(&mut board);
+        Board::print_board(&mut board);
+        Board::update_player_position(&mut board);
+        Board::remove_apples(&mut board);
+        if Board::no_apples_left(&board) {
             print_congrats_and_restart();
         }
     }
